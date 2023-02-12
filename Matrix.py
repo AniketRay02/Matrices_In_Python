528dafa801bf83593d01ee59bbc3e02b7e0cc9a0
AniketRay02 <124735905+AniketRay02@users.noreply.github.com>
2023-02-12 19:10:45 +0100

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -40,3 +40,20 @@ def matmul(A, B):  # Defining function matmul that takes two matrices 'A' and 'B
                 C[i][j] += A[i][k] * B[k][j]
     # Performs the matrix multiplication
     return C
+
+def invert(A):
+    a, b, c, d, = A[0][0], A[0][1], A[1][0], A[1][1]
+    determinant = a * d - b * c
+    if determinant == 0:
+        raise ValueError
+    return [[d / determinant, -b / determinant], [-c / determinant, a / determinant]]
+
+
+def loadtxt(filename):
+    data = []
+    with open(filename) as i:
+        for line in i:
+            line_data = [float(x) for x in line.split()]
+            data.append(line_data)
+    return data
+
